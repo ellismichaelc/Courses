92d9f262a04a1b18c1ba8d5d67435246e1bf77b2
Michael Ellis <whitem1ke88@gmail.com>
2015-10-20 19:36:18 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>CSS SELECTORS LESSON #1 - 10.20.15</title>
+    
+        <style type="text/css">
+            
+            body {
+                background: #
+            }
+            
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>
